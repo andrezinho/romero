25f08ee173ffd2b60fc6160b362522db01437e42
Cachete <ntorres-91@hotmail.com>
2013-07-31 01:11:44 -0500

    New TIPO MADERA
    TIPO MADERA 100%

---
+++ model/tipomadera.php
@@ -0,0 +1,93 @@
+<?php
+include_once("Main.php");
+class TipoMadera extends Main
+{    
+    function indexGrid($page,$limit,$sidx,$sord,$filtro,$query)
+    {
+        $offset = ($page-1)*$limit;
+        $query = "%".$query."%";
+        $sql = "SELECT
+            t.idtipomadera,
+            t.descripcion,
+            t.estado
+            FROM
+            produccion.tipomadera AS t";
+      
+        if($filtro!="") //$sql .= " WHERE ".$filtro." ilike :query ";
+        $sql .= " where cast(".$filtro." as varchar) ilike :query ";
+
+        $sql .= " order by {$sidx} {$sord}
+                 limit {$limit}
+                 offset  {$offset} "; 
+        
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(':query',$query,PDO::PARAM_STR);
+        $stmt->execute();
+        
+        $responce->records = $stmt->rowCount();
+        $responce->page = $page;
+        $responce->total = "1";        
+        $i = 0;
+        foreach($stmt->fetchAll() as $i => $row)
+        {
+            $responce->rows[$i]['id']=$row[0];
+            $responce->rows[$i]['cell']=array($row[0],$row[1],$row[2]);
+            $i ++;
+        }
+        return $responce;
+    }
+
+    function edit($id)
+    {
+        $stmt = $this->db->prepare("SELECT * FROM produccion.tipomadera WHERE idtipomadera = :id");
+        $stmt->bindParam(':id', $id , PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetchObject();
+    }
+    
+    function insert($_P ) 
+    {
+        $stmt = $this->db->prepare("INSERT INTO produccion.tipomadera(descripcion,estado)
+                                    values(:p1,:p2) ");
+               
+        $stmt->bindParam(':p1', $_P['descripcion'] , PDO::PARAM_STR);    
+        $stmt->bindParam(':p2', $_P['activo'] , PDO::PARAM_INT);        
+        
+        $p1 = $stmt->execute();
+        $p2 = $stmt->errorInfo();
+        
+        return array($p1 , $p2[2]);
+        
+    }
+
+    function update($_P ) 
+    {
+        $sql = "UPDATE produccion.tipomadera 
+                set  descripcion=:p1,                    
+                    estado=:p2
+                                   
+                WHERE idtipomadera = :idtipomadera";
+        $stmt = $this->db->prepare($sql);
+            
+            $stmt->bindParam(':p1', $_P['descripcion'] , PDO::PARAM_STR);            
+            $stmt->bindParam(':p2', $_P['activo'] , PDO::PARAM_INT);
+            
+            $stmt->bindParam(':idtipomadera', $_P['idtipomadera'] , PDO::PARAM_INT);
+
+
+        $p1 = $stmt->execute();
+
+        $p2 = $stmt->errorInfo();
+        return array($p1 , $p2[2]);
+    }
+    
+    function delete($p) 
+    {
+        $stmt = $this->db->prepare("DELETE FROM produccion.tipomadera WHERE idtipomadera = :p1");
+        $stmt->bindParam(':p1', $p, PDO::PARAM_INT);
+        $p1 = $stmt->execute();
+        $p2 = $stmt->errorInfo();
+        return array($p1 , $p2[2]);
+    }
+}
+?>
