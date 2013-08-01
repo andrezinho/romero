b59e1c9a861c7f8d2fa59dc625df2f8320ca8690
Cachete <ntorres-91@hotmail.com>
2013-07-31 21:31:51 -0500

    New madera
    madera 80%

---
+++ controller/maderaController.php
@@ -0,0 +1,100 @@
+<?php
+require_once '../lib/controller.php';
+require_once '../lib/view.php';
+require_once '../model/madera.php';
+
+class MaderaController extends Controller 
+{   
+    var $cols = array(
+                        1 => array('Name'=>'Codigo','NameDB'=>'m.idmadera','align'=>'center','width'=>50),
+                        2 => array('Name'=>'Descripcion','NameDB'=>'t.descripcion','width'=>250,'search'=>true),
+                        3 => array('Name'=>'Unidad Medida','NameDB'=>'u.descripcion','search'=>true),
+                        4 => array('Name'=>'Precio Unitario','NameDB'=>'m.precio_unitario'),
+                        5 => array('Name'=>'Stok','NameDB'=>'m.stock'),
+                        6 => array('Name'=>'Estado','NameDB'=>'m.estado','align'=>'center','width'=>70)
+                     );
+    public function index() 
+    {
+        $data = array();                               
+        $data['colsNames'] = $this->getColsVal($this->cols);
+        $data['colsModels'] = $this->getColsModel($this->cols);        
+        $data['cmb_search'] = $this->Select(array('id'=>'fltr','name'=>'fltr','text_null'=>'','table'=>$this->getColsSearch($this->cols)));
+        $data['controlador'] = $_GET['controller'];
+
+        //(nuevo,editar,eliminar,ver)
+        $data['actions'] = array(true,true,true,false);
+
+        $view = new View();
+        $view->setData($data);
+        $view->setTemplate('../view/_indexGrid.php');
+        $view->setlayout('../template/layout.php');
+        $view->render();
+    }
+    public function indexGrid() 
+    {
+        $obj = new Madera();        
+        $page = (int)$_GET['page'];
+        $limit = (int)$_GET['rows']; 
+        $sidx = $_GET['sidx'];
+        $sord = $_GET['sord'];
+        $filtro = $this->getColNameDB($this->cols,(int)$_GET['f']);        
+        $query = $_GET['q'];
+        if(!$sidx) $sidx = 1;
+        if(!$limit) $limit = 10;
+        if(!$page) $page = 1;
+        echo json_encode($obj->indexGrid($page,$limit,$sidx,$sord,$filtro,$query));
+    }
+    
+    public function create() 
+    {
+        $data = array();
+        $view = new View();
+        $data['idtipomadera'] = $this->Select(array('id'=>'idtipomadera','name'=>'idtipomadera','table'=>'produccion.vista_tipomadera','code'=>$obj->idtipomadera));
+        $data['idunidad_medida'] = $this->Select(array('id'=>'idunidad_medida','name'=>'idunidad_medida','table'=>'vista_unidadmedida','code'=>$obj->idunidad_medida));
+        $view->setData($data);
+        $view->setTemplate( '../view/madera/_form.php' );
+        echo $view->renderPartial();
+    }
+
+    public function edit() 
+    {
+        $obj = new Madera();
+        $data = array();
+        $view = new View();
+        $obj = $obj->edit($_GET['id']);
+        $data['obj'] = $obj;
+        $data['idtipomadera'] = $this->Select(array('id'=>'idtipomadera','name'=>'idtipomadera','table'=>'produccion.vista_tipomadera','code'=>$obj->idtipomadera));
+        $data['idunidad_medida'] = $this->Select(array('id'=>'idunidad_medida','name'=>'idunidad_medida','table'=>'vista_unidadmedida','code'=>$obj->idunidad_medida));
+        $view->setData($data);
+        $view->setTemplate( '../view/madera/_form.php' );
+        echo $view->renderPartial();
+    }
+    public function save()
+    {
+        $obj = new Madera();
+        $result = array();        
+        if ($_POST['idmadera']=='') 
+            $p = $obj->insert($_POST);                        
+        else         
+            $p = $obj->update($_POST);                                
+        if ($p[0])                
+            $result = array(1,'');                
+        else                 
+            $result = array(2,$p[1]);
+        print_r(json_encode($result));
+
+    }
+    public function delete()
+    {
+        $obj = new Madera();
+        $result = array();        
+        $p = $obj->delete($_GET['id']);
+        if ($p[0]) $result = array(1,$p[1]);
+        else $result = array(2,$p[1]);
+        print_r(json_encode($result));
+    }
+    
+    }
+ 
+
+?>
---
+++ view/madera/_form.php
@@ -0,0 +1,44 @@
+<?php  include("../lib/helpers.php"); 
+       include("../view/header_form.php");
+       
+?>
+<div style="padding:10px 20px">
+<form id="frm" >
+        <input type="hidden" name="controller" value="Madera" />
+        <input type="hidden" name="action" value="save" />
+
+        <label for="idmadera" class="labels">Codigo:</label>
+        <input type="text" id="idmadera" name="idmadera" class="text ui-widget-content ui-corner-all" style=" width: 200px; text-align: left;" value="<?php echo $obj->idmadera; ?>" readonly />
+        
+        <label for="idtipomadera" class="labels">Tipo Madera:</label>
+        <?php echo $idtipomadera; ?>
+        <br/>
+
+        <label for="idunidad_medida" class="labels">Unidad Medida:</label>
+        <?php echo $idunidad_medida; ?>
+        
+        <label for="tipoproducto" class="labels">Tipo Producto:</label>
+   		<input id="tipoproducto" name="tipoproducto" class="text ui-widget-content ui-corner-all" style=" width: 200px; text-align: left;" value="<?php echo $obj->tipoproducto; ?>"  />
+        <br/>
+
+        <label for="precio_unitario" class="labels">Precio Uunitario:</label>
+   		<input type="text" id="precio_unitario" name="precio_unitario" class="text ui-widget-content ui-corner-all" style=" width: 200px; text-align: left;" value="<?php echo $obj->precio_unitario; ?>" />
+        
+        <label for="stock" class="labels">Stock:</label>
+   		<input type="text" id="stock" name="stock" class="text ui-widget-content ui-corner-all" style=" width: 200px; text-align: left;" value="<?php echo $obj->stock; ?>" />
+        <br/>
+        
+        <label for="estado" class="labels">Activo:</label>
+            <div id="estados" style="display:inline">
+            <?php                   
+                if($obj->estado==1 || $obj->estado==0)
+                {
+                    if($obj->estado==1){$rep=1;}
+                    else {$rep=0;}
+                }
+                else {$rep = 1;}                    
+                    activo('activo',$rep);
+            ?>
+    </div>
+</form>
+</div>
