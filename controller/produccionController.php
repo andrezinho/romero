e8f28edeee0c2b78f3552324deecf540294dcf0e
andrezinho <andres.gm15@gmail.com>
2013-09-28 00:00:05 -0500

    Yata casi

--- controller/produccionController.php
+++ controller/produccionController.php
@@ -2,6 +2,7 @@
 require_once '../lib/controller.php';
 require_once '../lib/view.php';
 require_once '../model/produccion.php';
+require_once "../lib/Excel/reader.php";
 class ProduccionController extends Controller 
 {   
     var $cols = array(
@@ -252,6 +253,78 @@ class ProduccionController extends Controller
         $view->setTemplate( '../view/produccion/_rptDetalle.php' );
         $view->setLayout( '../template/list.php' );
         $view->render();
+    }
+    public function migrador()
+    {
+        $obj = new Produccion();
+        $data = array();
+        $view = new View();
+        $data['almacend'] = $this->Select(array('id'=>'idalmacen','name'=>'idalmacen','text_null'=>'','table'=>'produccion.almacenes','width'=>'160px'));                
+        $view->setData($data);
+        $view->setTemplate( '../view/produccion/migrador.php' );
+        $view->setLayout( '../template/list.php' );
+        $view->render();
+    }
+    public function migrar()
+    {
+        $obj = new Produccion();
+        $allow_url_override = 1;
+        if(!$allow_url_override || !isset($file_to_include))
+        {
+            $file_to_include = $_FILES["archivo"]["tmp_name"];
+        }
+        if(!$allow_url_override || !isset($max_rows))
+        {
+            $max_rows = 0; //USE 0 for no max
+        }
+        if(!$allow_url_override || !isset($max_cols))
+        {
+            $max_cols = 5; //USE 0 for no max
+        }
+        if(!$allow_url_override || !isset($debug))
+        {
+            $debug = 0;  //1 for on 0 for off
+        }
+        if(!$allow_url_override || !isset($force_nobr))
+        {
+            $force_nobr = 1;  //Force the info in cells not to wrap unless stated explicitly (newline)
+        }
+        $data = new Spreadsheet_Excel_Reader();
+        $data->setOutputEncoding('CP1251');
+        $data->read($file_to_include);
+        error_reporting(E_ALL ^ E_NOTICE);
+        //die($_SESSION['idusuario']."SS");
+        $_P['fechai'] = date('Y-m-d');
+        $_P['fechaf'] = date('Y-m-d');        
+        $_P['idpersonal'] = $_SESSION['idusuario'];
+
+        $_P['idalmacen']  = $_POST['idalmacen'];
+        $_P['idalmacend']  = $_POST['idalmacen'];
+        $_P['idproducciontipo'] = 5;
+
+        $cont = 0;
+        $_P['prod'] = array();        
+        $_P['prod']['idsps']=array();
+        $_P['prod']['cantidad']=array();
+        $_P['prod']['estado']=array();
+        $c = 0;
+        $_P['idreferencia'] = "";
+        
+        for($i=3;$i<=$data->sheets[0]['numRows'];$i++)
+        {
+            if($data->sheets[0]['cells'][$i][8]!="")
+            {
+                $c = $c+1;
+                $_P['prod']['idalmacen'][] = $_POST['idalmacen'];
+                $_P['prod']['idsps'][] = $data->sheets[0]['cells'][$i][8];
+                $_P['prod']['cantidad'][] = $data->sheets[0]['cells'][$i][4];
+                $_P['prod']['estado'][] = true;
+            }
+        }
+        $_P['prod']['item'] = $c;
+        //echo $c;
+        $obj->InsertProduccion($_P);
+        echo "Se han migrado ".$c." Registros ";
     }
 }
 ?>
